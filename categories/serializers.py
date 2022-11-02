a5de5971c8f225797e70b4efd538c90c696a9220
dongyu-youn <donghe1472@naver.com>
2022-11-02 22:03:57 +0900

    [Feat]room detail까지 api 작성완료

--- categories/serializers.py
+++ categories/serializers.py
@@ -5,4 +5,8 @@ from .models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = (
+            "name",
+            "kind",
+        )
+
