a194a94b8787dc53a94c75cf941ed4e80d72ecb2
dongyu-youn <donghe1472@naver.com>
2022-11-02 10:22:40 +0900

    category쪽viewset으로 변경

--- categories/urls.py
+++ categories/urls.py
@@ -2,6 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Categories.as_view()),
-    path("<int:pk>", views.CategoryDetail.as_view()),
+    path(
+        "",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),
+    path(
+        "<int:pk>",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "retrieve", 
+                # 전체에서 한개를 주는 메소드
+                "put": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),
 ]
